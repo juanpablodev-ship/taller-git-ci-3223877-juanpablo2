6c4d79e1a5b89346cd67c17273d03077814ab2bf
juanSA26273 <juanpablosernaarboleda464@gmail.com>
2025-12-02 15:59:27 -0500

    ultimo intento3

--- tests/rest.test.js
+++ tests/rest.test.js
@@ -1,7 +1,7 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
 function rest(a, b) {
-return a - b;
+  return a - b;
 }
 test('rest 2 -3 = -1', () => {
   assert.equal(rest(2, 3), -1);
